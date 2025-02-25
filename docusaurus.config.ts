c013619db8ded8dd6da74d93f33a1c81e289715b
rick <jkxyx205@163.com>
2025-02-25 23:02:00 +0800

    Update GitHub link in docusaurus.config.ts to point to the correct repository

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -94,7 +94,7 @@ const config: Config = {
         },
         { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/jkxyx205/docs-test',
           label: 'GitHub',
           position: 'right',
         },
